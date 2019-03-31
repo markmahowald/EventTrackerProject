96a8fbf54fbdbb21200de93d0d41194b6b87e122
markmahowald <markmahowald0328@gmail.com>
2019-03-31 10:33:30 -0600

    Posting a new transaction is now possible

--- expenseREST/bin/main/static/js/script.js
+++ expenseREST/bin/main/static/js/script.js
@@ -17,6 +17,8 @@ function init() {
 		}
 	}
 	xhr.send();
+
+	submitForm();
 }
 
 function displayAllTransactions(transactions) {
@@ -33,9 +35,60 @@ function displayAllTransactions(transactions) {
 		let li = document.createElement('li');
 
 		if (transactions[i].incomeOrExpense === 'expense') {
-			li.textContent = 'Category: '+ transactions[i].category+ '-> - $ '+(transactions[i].amount);
+			li.setAttribute('style', 'color: red');
+			li.textContent = 'Date: ' + transactions[i].date + ' Category: '
+					+ transactions[i].category + '-> - $ '
+					+ (transactions[i].amount)+ ' - '+ transactions[i].source;;
 		} else {
-			li.textContent = 'Category: '+ transactions[i].category+ '-> $ '+(transactions[i].amount);		}
+			li.setAttribute('style', 'color: green');
+			li.textContent = 'Date: ' + transactions[i].date + ' Category: '
+					+ transactions[i].category + '-> $ '
+					+ (transactions[i].amount)+ ' - '+ transactions[i].source;
+		}
 		ul.appendChild(li);
 	}
-}
+};
+
+function submitForm() {document.submitTransactionForm.saveTrans.addEventListener('click',function(e) {
+		e.preventDefault();
+		let form = document.forms.submitTransactionForm;
+		let transaction = {
+			incomeOrExpense:  form.incomeOrExpense.value,
+			date : form.transDate.value,
+			category: form.category.value,
+			amount: form.amount.value,
+			source: form.source.value,
+			
+//			multiple accounts will be implemented in a future release. 
+			account: { id: 1,
+	            name: "checking",
+	            total: 3000
+	            }
+		};
+		console.log (transaction);
+		if (!(transaction.date === null || transaction.category === null || transaction.amount === null)) {
+	
+//			console.log(transaction.date);
+			let xhr = new XMLHttpRequest();
+			let url = 'api/transactions/';
+			xhr.open('POST', url);
+			xhr.setRequestHeader("Content-type","application/json");
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4) {
+					if (xhr.status == 200 || xhr.status == 201) {
+						var data = JSON.parse(xhr.responseText);
+					}
+					// TODO - write a post failure notification.
+				}
+			}
+			let transactionJSON = JSON.stringify(transaction);
+			xhr.send(transactionJSON);
+		} else {
+			console.log("POST request failed.");
+		      
+
+		}
+
+	});
+	
+};
